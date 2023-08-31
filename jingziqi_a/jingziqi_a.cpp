f2b3470fa92433e73df1d39f41ab207492b38a78
DMP Eizens <xk3623@126.com>
2023-08-31 18:53:55 +0800

    胜负判定函数
    梁策编写，刘睿达代传
    
    Co-Authored-By: LCWZNB <142365306+lcwznb@users.noreply.github.com>

--- jingziqi_a/jingziqi_a.cpp
+++ jingziqi_a/jingziqi_a.cpp
@@ -258,7 +258,18 @@ void print(int result)
     return;
 }
 void ai(int round){}
-bool win(){}
+bool win(){
+    return (board[0] == board[1] && board[0] == board[2] && board[0] != 0)
+        || (board[3] == board[4] && board[3] == board[5] && board[3] != 0)
+        || (board[6] == board[7] && board[6] == board[8] && board[6] != 0)
+
+        || (board[0] == board[3] && board[0] == board[6] && board[0] != 0)
+        || (board[1] == board[4] && board[1] == board[7] && board[1] != 0)
+        || (board[2] == board[5] && board[2] == board[8] && board[2] != 0)
+
+        || (board[0] == board[4] && board[0] == board[8] && board[0] != 0)
+        || (board[2] == board[4] && board[2] == board[6] && board[2] != 0);
+}
 
 int main()
 {
